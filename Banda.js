3356ff4070f9a5d58ae692bf17c625d5d855307d
Kenedy-silva2 <gustavodasilvama10@gmail.com>
2023-12-09 15:12:16 -0300

    Banda

---
+++ Banda.js
@@ -0,0 +1,14 @@
+class Banda{
+    constructor(nome, albuns, genero){
+        this.nome = nome,
+        this.albuns = albuns,
+        this.genero = genero
+    }
+
+    AdicionarAlbum(){
+        console.log("Album adicionado")
+    }
+    RemoverAlbum(){
+        console.log("Album removido")
+    }
+}
--- Encomenda.js
+++ Encomenda.js
@@ -14,6 +14,7 @@ class Encomenda{
         console.log("calcular frete")
     }
 
+    
 
 
 }
