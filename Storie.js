297bbcc8012e546a54f3f1c763d77fce604ab893
Kenedy-silva2 <gustavodasilvama10@gmail.com>
2023-12-09 18:00:05 -0300

    Storie

---
+++ Storie.js
@@ -0,0 +1,19 @@
+class Storie{
+    constructor(marcar, descricao, titulo, localidade, video){
+        this.marcar = marcar, 
+        this.descricao = descricao, 
+        this.titulo = titulo, 
+        this.localidade = localidade, 
+        this.video = video
+    }
+
+    Editar(){
+        console.log("Editado")
+    }
+    Enviar(){
+        console.log("Enviar")
+    }
+    Deletar(){
+        console.log("Deletado")
+    }
+}
