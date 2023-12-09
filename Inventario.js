81b5ed8bf7cba7f342e5664246a2ea99f0f90390
Kenedy-silva2 <gustavodasilvama10@gmail.com>
2023-12-09 17:30:02 -0300

    Inventario

---
+++ Inventario.js
@@ -0,0 +1,18 @@
+class Inventario{
+    constructor(player, items){
+        this.player = player,
+        this.items = items
+    }
+    ChecarItem(){
+        console.log("Isto é uma blusa!")
+    }
+    JogarItem(){
+        console.log("item deletado")
+    }
+    UsarItem(){
+        console.log("Usando...")
+    }
+    Vestir(){
+        console.log("Armadura colocada")
+    }
+}
