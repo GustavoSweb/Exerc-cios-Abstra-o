7b356efee44e7c90c1a2b7f2b5bf79ca3e027e89
Kenedy-silva2 <gustavodasilvama10@gmail.com>
2023-12-10 14:14:54 -0300

    Busca feed

---
+++ Busca.js
@@ -0,0 +1,15 @@
+class Busca{
+    constructor(texto, tipo, usuario, resultado){
+        this.texto = texto, 
+        this.tipo = tipo, 
+        this.usuario = usuario,
+        this.resultado = resultado
+    }
+    Atualizar(){
+        console.log("Atualizando.. ")
+    }
+    CarregarFeed(){
+        console.log("Feed carregado (images)")
+    }
+
+}
